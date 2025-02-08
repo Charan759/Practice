3d9a9c747aaf3906b197a326fc678bdc34587c98
charan <charan.vangara34@gmail.com>
2025-02-08 22:21:58 +0530

    Added Hooks.jsx

---
+++ Hooks.jsx
@@ -0,0 +1,18 @@
+// react hooks
+// 1. use State
+
+
+import { useState } from 'react';
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  );
+}
+
+export default Counter;
